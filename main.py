b40b650d68119cd4041bcfc57459c92227a428c3
Son Luong Ngoc (Lazada Group) <son.luongngoc@lazada.com>
2018-06-16 16:35:34 +0700

    added history persistent to disk

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from instagram_web_api import Client
 import requests
+import time
 
 INSTAGRAM_BASE_URL = "https://www.instagram.com/p/"
 DINGTALK_BASE_URL = "https://oapi.dingtalk.com/robot/send?access_token="
 
 # REPLACE THIS WITH YOURS DINGDING CHATBOT
-ACCESS_TOKEN = "123123"
+ACCESS_TOKEN = "7c86f414bea82d41cf52f905de947663a4adfcd4854270a57eaf834ae223d552"
 INSTAGRAM_TAG = "selfie"
 
 
@@ -33,16 +34,30 @@ def send2Ding(postId, instagramPic, shortCode):
 
 def main():
     web_api = Client(auto_patch=True, drop_incompat_keys=False)
-    feed = web_api.tag_feed(INSTAGRAM_TAG)
-    feed = feed["data"]["hashtag"]["edge_hashtag_to_media"]["edges"]
+    while True:
+        old_posts = []
+        posts_file = open("./old_posts.txt", "a+")
+        for line in posts_file:
+            old_posts.append(line)
 
-    for post in feed:
-        post = post["node"]
-        postId = post["id"]
-        picUrl = post["display_url"]
-        shortCode = post["shortcode"]
-        if not post["is_video"]:
-            send2Ding(postId, picUrl, shortCode)
+        feed = web_api.tag_feed(INSTAGRAM_TAG)
+        feed = feed["data"]["hashtag"]["edge_hashtag_to_media"]["edges"]
+
+        for post in feed:
+            post = post["node"]
+            postId = post["id"]
+            picUrl = post["display_url"]
+            shortCode = post["shortcode"]
+
+            if postId in old_posts:
+                print "{} already exist".format(postId)
+            else:
+                posts_file.write(postId+"\n")
+                old_posts.append(postId)
+                if not post["is_video"]:
+                    send2Ding(postId, picUrl, shortCode)
+        posts_file.close()
+        time.sleep(60)
 
 
 if __name__ == '__main__':
